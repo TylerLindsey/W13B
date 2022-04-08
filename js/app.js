42ea2c51e7cf3c4af475fd0bf05eec226a4ae0e7
tygoli <tylerlindsey403@gmail.com>
2022-04-08 03:03:14 -0600

    added comments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-
-
+// The function below sets numUser as a JS variable to reference the HTML dropdown options of the same name. The If statement links the number on the drop down to the number in the API to display the correct information. The else function catches if there is an error. 
 function playerSelector(){
   console.log(document.getElementById('numUsers').value);
   let numUsers = document.getElementById('numUsers').value;
-  // after talking with Mark I had trouble getting it to display 1-4 in the if, now all the drop down options work because i turned (numUsers == '1') into (numUsers == '1', '2') and I have no idea why. I thought if anything that 1 and 2 would work from the dropdown and 3 and 4 would still return the error, but they all return 'insert games from API'
   if (numUsers == '1', '2'){
     // console.log(numUsers + ' if');
     // document.getElementById('answer').innerHTML = 'supposed to be activity';
@@ -20,6 +18,7 @@ function playerSelector(){
   }
 }
 
+// if the API was successful the data from activity is logged into the console and displayed on the webpage.
 function successFunction(response){
   console.log(response.data.activity);
   document.getElementById('answer').innerHTML = response.data.activity;
@@ -29,15 +28,6 @@ function failFunction(error){
   console.log(error);
 }
 
-function randomize(){
-  document.getElementById('activity');
-}
-
+// to make it so the user can just 'reroll' an activity I just linked the same event to a different button. So both buttons are connected to the same event.
 document.getElementById('activity').addEventListener('click', playerSelector); 
 document.getElementById('randomizer').addEventListener('click', playerSelector); 
-
-
-// where does .then/.catch go? They go after the axios request, does that mean each axios needs it's own then/catch? and does each requestion also need it's own success/fail function? or should i have the 
-// I know that with the else/if that numUsers == x doesnt work, but is everything else going to work? 
-// im having trouble displaying what is on the API on the screen
-// I guess im running out of ideas and want to know if i am at least going down the rght path
